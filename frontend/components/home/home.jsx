74531182b089de9f824a0a9825e46135e14654e7
Vu Pham <vutpham@outlook.com>
2017-08-23 22:00:21 -0700

    Testing background iamges

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,7 +4,7 @@ class Home extends React.Component {
   render(){
     return(
       <div id="home">
-        <img src="./assets/yipeng_cover.jpg" id="main-home-img"></img>
+        <img src="http://cdn.wallpapersafari.com/97/60/VKjztJ.jpg" id="main-home-img"></img>
           <div id="main-img-container"></div>
       </div>
     );
