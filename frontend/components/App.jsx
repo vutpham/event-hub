ece4d98e8bc94765a9e82c3370f035ac4c55b863
Vu Pham <vutpham@outlook.com>
2017-08-23 21:19:55 -0700

    Create footer

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { AuthRoute } from '../util/route_util';
 import SessionFormContainer from './auth/auth_form_container';
 import GreetingContainer from './auth/greeting_container';
 import NavBarContainer from './nav/nav_bar_container';
 import Home from './home/home';
+import Footer from './footer';
 
 
 const App = () => (
@@ -12,7 +13,10 @@ const App = () => (
     <header>
       <NavBarContainer />
     </header>
-    <Route exact path="/" component={Home} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+    <Footer />
   </div>
 );
 
---
+++ frontend/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = (props) => (
+  <div className='footer'>
+      <div>
+        <small>Inspired by Eventbrite, made by Vu T. Pham</small>
+        <br/>
+        <small>For Portfolio Purposes Only</small>
+      </div>
+      <div className='icon-box'>
+        <a href='mailto:vutpham@outlook.com'><i className='fa fa-envelope-o' aria-hidden='true'></i></a>
+        <a href='https://github.com/vutpham/event-hub' target='_blank'><i className='fa fa-github' aria-hidden='true'></i></a>
+        <a href='https://www.linkedin.com/in/vutpham/' target='_blank'><i className='fa fa-linkedin' aria-hidden='true'></i></a>
+      </div>
+  </div>
+);
+
+export default Footer;
