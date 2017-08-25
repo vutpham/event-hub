163b60651d81b50ace9919e44fedb8f97dbe568d
Vu Pham <vutpham@outlook.com>
2017-08-24 19:04:18 -0700

    Style event detail page

--- frontend/components/events/event_detail.jsx
+++ frontend/components/events/event_detail.jsx
@@ -16,17 +16,44 @@ class EventDetail extends React.Component{
   }
 
   render(){
-    const {title, short_description, full_description, image_url, host} = this.props.eventDetails;
+    const {title, short_description, full_description, image_url, host,
+           price, date, venue, address} = this.props.eventDetails;
     return(
       <div id="event-details">
         <div id="event-details-left-col">
-          <h1>{title}</h1>
+          <h1>{title} &nbsp; <i className="fa fa-bookmark-o" aria-hidden="true"></i></h1>
           <h2>{short_description}</h2>
           <h3>Hosted By: {host}</h3>
           <p>Details: {full_description}</p>
         </div>
         <div id="event-details-right-col">
-          <img className="event-detail-img" src={image_url}></img>
+
+            <img className="event-detail-img" src={image_url}></img>
+            <span className="event-info">
+
+              <div className="ticket-info">
+                <div>${price}</div>
+                <button className="buy-tickets">Tickets</button>
+              </div>
+
+              <div className="event-more-info">
+                <div className="event-more-info-attr">When:</div>
+                <div className="event-more-info-data">{date}</div>
+              </div>
+
+              <div className="event-more-info">
+                <div className="event-more-info-attr">How Long:</div>
+                <div className="event-more-info-data">FILL IN TIME</div>
+              </div>
+
+              <div className="event-more-info">
+                <div className="event-more-info-attr">Where: </div>
+                <span className="event-more-info-data">
+                  <div>{venue}</div>
+                  <div>{address}</div>
+                </span>
+              </div>
+          </span>
         </div>
       </div>
     );
