693edef33d56e3dd1b7c34ae054918af73462a12
Vu Pham <vutpham@outlook.com>
2017-08-26 01:40:06 -0700

    Add styling to homepage

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import EventSliderContainer from '../events/index_event/event_slider_container';
 
 class Home extends React.Component {
   render(){
     return(
       <div id="home">
-        <img src="http://cdn.wallpapersafari.com/97/60/VKjztJ.jpg" id="main-home-img"></img>
-          <div id="main-img-container"></div>
+        <img src="http://cdn.wallpapersafari.com/97/60/VKjztJ.jpg"
+             id="main-home-img">
+        </img>
+        <h1 className='trending-events-title'>
+          Trending Events4U
+        </h1>
+        <EventSliderContainer />
+        <div className="see-more-button-container">
+          <button className='see-more-button'>
+            See More Events
+          </button>
+        </div>
       </div>
     );
   }
