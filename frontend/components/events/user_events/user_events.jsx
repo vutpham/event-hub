e84326881da03e46a0b3f70765a15fae73a9e85b
Vu Pham <vutpham@outlook.com>
2017-10-17 22:46:40 -0700

    Add FA icons to account page

--- frontend/components/events/user_events/user_events.jsx
+++ frontend/components/events/user_events/user_events.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
@@ -28,28 +29,41 @@ class UserEvents extends React.Component{
     return () => this.props.history.push(path);
   }
 
+
+
   render(){
     return(
       <div id="account-page">
         <h1>Welcome, {this.props.currentUser.username}</h1>
         <header className="account-page-header">
-          <span
+          <nav
             className={"bookmarks-toggle" + this.dynamicSelector(1)}
             onClick={this.goTo('/user-events/bookmarks')}>
-            My Saved Events
-          </span>
-          <span
+            <span>My Saved Events</span>
+            <span className="fa-stack fa-lg">
+              <i className="fa fa-circle-o fa-stack-2x"></i>
+              <i className="fa fa-bookmark fa-stack-1x"></i>
+            </span>
+          </nav>
+          <nav
             className={"tickets-toggle" + this.dynamicSelector(2)}
             onClick={this.goTo('/user-events/tickets')}>
-            My Tickets
-          </span>
-          <span className={"hosted-events-toggle" + this.dynamicSelector(3)}
+            <span>My Tickets</span>
+            <span className="fa-stack fa-lg">
+              <i className="fa fa-circle-o fa-stack-2x"></i>
+              <i className="fa fa-ticket fa-stack-1x"></i>
+            </span>
+          </nav>
+          <nav className={"hosted-events-toggle" + this.dynamicSelector(3)}
             onClick={this.goTo('/user-events/hosted-events')}>
-            My Hosted Events
-          </span>
+            <span>My Hosted Events</span>
+            <span className="fa-stack fa-lg">
+              <i className="fa fa-circle-o fa-stack-2x"></i>
+              <i className="fa fa-user fa-stack-1x"></i>
+            </span>
+          </nav>
         </header>
         <div className="account-page-display">
-
         </div>
       </div>
     );
